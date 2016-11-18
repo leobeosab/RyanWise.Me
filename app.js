ada1d04ed2676a5995af760dce7a595e1942b61a
Ryan Wise <ryan.wise@wiseweb-design.com>
2016-11-17 21:01:04 -0500

    maion page update / style

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
-var http = require
+var http = require("http");
 var express = require("express");
 var app = express();
+var path = require("path");
 var routes = require("./routes")(app);
+
+app.use('/img',express.static(path.join(__dirname, 'www/img')));
+app.use('/js',express.static(path.join(__dirname, 'www/js')));
+app.use('/style',express.static(path.join(__dirname, 'www/style')));
 
 http.createServer(app).listen(80, function() {
   console.log("Server started");
---
+++ run.bat
@@ -0,0 +1,2 @@
+cd C:\Users\Ryan\Desktop\"NodeJS - JS"\wisewebdesign
+node app.js
