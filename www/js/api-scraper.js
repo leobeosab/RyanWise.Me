4643ab6ed1a545e9ab4931b453f3f8c3e461b50e
Ryan Wise <ryan.wise@wiseweb-design.com>
2016-11-22 23:07:50 -0500

    update for reddit

--- www/js/api-scraper.js
+++ www/js/api-scraper.js
@@ -1,13 +1,17 @@
 'use strict';
 
 //Should be fairly explanitory
-function getGithubDetails(callback) {
+function getApiData(callback, link) {
   let data = undefined;
   //initial ajax request
   $.ajax({
+    crossDomain: true,
     method: "GET",
     contentType: "JSON",
-    url: "https://api.github.com/users/leobeosab/events"
+    url: link,
+    beforeSend: function(xhr) {
+      xhr.withCredentials = true;
+    }
   }).done((resp) => callback(resp)).fail((err) => callback(err));
   //make sure we have the data and return it
 }
--- www/js/main.js
+++ www/js/main.js
@@ -13,7 +13,7 @@ function init() {
   //vue component setups
   Vue.component('git-event', {
       props:['event'],
-      template: '<li> <span class="user">Ryan</span>: <a v-bind:href="event.repoUrl" target="_blank">{{event.message}}</a></li>'
+      template: '<li> <span class="date"> {{event.date}} </span>, <br> <a v-bind:href="event.repoUrl" target="_blank">{{event.message}}</a></li>'
   });
   vueElements.github = new Vue({
     el: "#github",
@@ -22,37 +22,79 @@ function init() {
     }
   });
 
+  Vue.component('hackerrank-challenge', {
+      props:['challenge'],
+      template: '<li> <span class="date"> {{challenge.date}} </span> - <span class="user"> Ryan </span> completed <a v:bind:href="challenge.url" target="_blank"> {{event.name}}</a></li>'
+  });
+  vueElements.hackerrank = new Vue({
+    el: "#hackerrank",
+    data: {
+      challengeList: []
+    }
+  })
+
   //get our gitdata
-  getGithubDetails(githubCallback);
+  getApiData(githubCallback, "https://api.github.com/users/leobeosab/events");
+  //get out hackerrank data doesn't allow cors
+  //getApiData(hackerrankCallback, "https://www.hackerrank.com/rest/hackers/ryan_wise_asm/recent_challenges?offset=0&limit=15")
 }
+
+const months = [
+  "January", "February", "March",
+  "April", "May", "June", "July",
+  "August", "September", "October",
+  "November", "December"
+];
 
 function githubCallback(data) {
   //prepare array
   let gitData = [];
   //format our data
   for (let key of data) {
+        const dateobj = new Date(key.created_at);
+        const date = months[dateobj.getMonth()] + " " + dateobj.getDate() +" " + dateobj.getFullYear();
+        const url =  "https://github.com/" + key.repo.name;
+        const name = key.repo.name.replace("leobeosab/", "");
         if (key.type == "PushEvent") {
         gitData.push( {
         action: "push",
-        repoUrl: "https://github.com/" + key.repo.name,
-        message: "pushed to " + (key.repo.name).replace("leobeosab/", "")
+        date: date,
+        repoUrl: url,
+        message: "pushed to " + name
         });
         } else if (key.type == "CreateEvent") {
           if (key.payload.ref_type == "repository") {
             gitData.push({
               action: "repo-create",
-              repoUrl: key.repo.url,
-              message: "created a new repository, " + (key.repo.name).replace("leobeosab/", "")
+              date: date,
+              repoUrl: url,
+              message: "created a new repository, " + name
             });
           } else if (key.payload.ref_type == "branch") {
             gitData.push({
               action: "branch-create",
-              repoUrl: key.repo.url,
-              message: "created a new branch, " + key.payload.ref + ", on " + (key.repo.name).replace("leobeosab/", "")
+              date: date,
+              repoUrl: url,
+              message: "created a new branch, " + key.payload.ref + ", on " + name
             });
           }
         }
     }
     //put it in a vue element
     vueElements.github.$data.eventList = gitData;
+}
+
+function hackerrankCallback(data) {
+  let hrData = [];
+  for (let key of data) {
+    const dateobj = new Date(key.created_at);
+    const date = months[dateobj.getMonth()] + " " + dateobj.getDate() +" " + dateobj.getFullYear();
+    const link = "https://hackerrank.com/" + key.url;
+    hrData.push({
+      name: key.name,
+      date: date,
+      link: link
+    });
+  }
+  vueElements.hackerrank.$data.challengeList = hrData;
 }
