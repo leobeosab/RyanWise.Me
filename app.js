5ed601762617a26cea76832e90b952e6df35413d
Ryan Wise <ryan.wise@wiseweb-design.com>
2017-02-12 17:34:31 -0500

    Redid website to be only the blog with minor css and html changes

--- app.js
+++ app.js
@@ -3,20 +3,8 @@ var express = require("express");
 var connect = require("connect");
 var app = express();
 var path = require("path");
-//var routes = require("./routes")(app);
-var vhost = require("vhost");
 
-function createVirtualHost(domain, dirPath) {
-  return vhost(domain, express.static( dirPath ));
-}
-
-
-//Vhosts
-var devHost = createVirtualHost("dev.ryanwise.me", "dev/www/");
-var defaultHost = createVirtualHost("localhost", "www/");
-
-app.use(devHost);
-app.use(defaultHost);
+app.use(express.static("www/blog/"))
 
 app.listen(3000, function() {
   console.log("Now listening on port :80");
