ba81f06ce2101385e927b68444181fbc59016cae
Ryan Wise <ryan.wise@wiseweb-design.com>
2017-02-12 22:42:58 +0000

    clean up on old ideas

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
 var http = require("http");
 var express = require("express");
-var connect = require("connect");
 var app = express();
 var path = require("path");
 
 app.use(express.static("www/blog/"))
 
-app.listen(3000, function() {
+app.listen(80, function() {
   console.log("Now listening on port :80");
 } );
--- dev/www/js/main.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-//init on document's ready
-$(document).ready(()=>init());
-
-function init() {
-  GitHubCalendar(".calendar", "leobeosab");
-}
